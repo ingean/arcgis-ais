f683392cc8e60b5cb9f729b10ff9082c4cd2464a
Inge Anundskås <42357272+ingean@users.noreply.github.com>
2022-02-04 14:40:54 +0100

    v2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,69 @@
 require([
   "esri/Map",
   "esri/views/MapView",
-  "esri/layers/ImageryTileLayer"
-], function (Map, MapView, ImageryTileLayer) {
-  const layer = new ImageryTileLayer({
-    //url: "https://tiledimageservices.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/NLDAS2011_daily_wind_magdir/ImageServer",
-    //url: "https://tiledimageservices.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/AIS_MagDir/ImageServer",
-    url: "https://tiledimageservices.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/fishnet_1km_conv_1000/ImageServer",
+  "esri/layers/FeatureLayer",
+  "esri/layers/ImageryTileLayer",
+  "esri/layers/GroupLayer",
+  "esri/rest/support/MultipartColorRamp",
+  "esri/rest/support/AlgorithmicColorRamp",
+  "esri/Color",
+  "esri/widgets/Legend",
+  "esri/widgets/Fullscreen"
+  
+], 
+
+function (
+  Map, 
+  MapView,
+  FeatureLayer, 
+  ImageryTileLayer,
+  GroupLayer,
+  MultipartColorRamp,
+  AlgorithmicColorRamp,
+  Color,
+  Legend,
+  Fullscreen) {
+
+  const colorRamp = new MultipartColorRamp({
+    colorRamps: [
+  
+
+      new AlgorithmicColorRamp({
+        fromColor: new Color([0, 0, 150, 255]),
+        toColor: new Color([3000, 0, 120, 255])
+      }),
+      new AlgorithmicColorRamp({
+        fromColor: new Color([3000, 0, 120, 255]),
+        toColor: new Color([5000, 100, 60, 255])
+      }),
+      new AlgorithmicColorRamp({
+        fromColor: new Color([5000, 100, 60, 255]),
+        toColor: new Color([10000, 170, 0, 255])
+      }),
+      new AlgorithmicColorRamp({
+        fromColor: new Color([10000, 170, 0, 255]),
+        toColor: new Color([45000, 255, 0, 255])
+      })
+    ]
+  });
+
+  // sea surface temperature, visualized with raster stretch renderer
+  const magnitudeLayer = new ImageryTileLayer({
+    url: "https://tiledimageservices.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/ais_2017_supply_count_wgs8/ImageServer",
+    //url: "https://tiledimageservices.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/ais_2017_count_wgs84/ImageServer",
+    renderer: {
+      colorRamp,
+      "computeGamma": false,
+      "gamma": [1],
+      "useGamma": false,
+      "stretchType": "min-max",
+      "type": "raster-stretch"
+    }
+  });
+
+  const directionLayer = new ImageryTileLayer({
+    url: "https://tiledimageservices.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/ais_2017_supply_wgs84/ImageServer",
+    //url: "https://tiledimageservices.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/ais_2017_wgs84/ImageServer",
     title: "Ship traffic",
     renderer: {
       type: "animated-flow", // autocasts to new AnimatedFlowRenderer
@@ -17,12 +74,22 @@ require([
       fadeDuration: 0.5,
       lineLength: 50
     },
-    effect: "bloom(2, 0.5px, 0)"
+    blendMode: "destination-in", // temperature layer will only display on top of this layer
   });
+
+  const magdirLayer = new GroupLayer({
+    effect: "bloom(2, 0.5px, 0.0)", // apply bloom effect to make the colors pop
+    layers: [magnitudeLayer, directionLayer]
+  });
+
+  const pilotLayer = new FeatureLayer({
+    title: "Farled",
+    url: "https://services.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/Farled/FeatureServer/0"
+  })
 
   const map = new Map({
     basemap: "dark-gray-vector",
-    layers: [layer]
+    layers: [magdirLayer]
   });
 
   const view = new MapView({
@@ -31,7 +98,24 @@ require([
     zoom: 9,
     center: [4.5, 59.3]
   });
-  view.ui.add("controls", "top-right");
+  
+  //view.ui.add("controls", "top-right"); # Controls for adjusting animated flow
+
+   // add legend for temperature layer
+   const legend = new Legend({
+    view: view,
+    layerInfos: [{
+      layer: magnitudeLayer,
+      title: "Antall observasjoner"
+    }]
+  });
+  view.ui.add(legend, "top-right");
+
+  // add fullscreen widget
+  const fullscreen = new Fullscreen({
+    view: view
+  });
+  view.ui.add(fullscreen, "top-left");
 
   document
     .getElementById("lineWidth")
